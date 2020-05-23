e0de0ad0ca5d4a13722671196165c9b78459819f
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-05-23 17:40:16 -0300

    added: Cliente List

--- controllers/ClienteController.js
+++ controllers/ClienteController.js
@@ -83,4 +83,50 @@ module.exports = (app) => {
         res.status(201).send(resp);
     });
 
+    // [GET] => /cliente
+    app.get(`/cliente`, async (req, res) => {
+        const { query } = req;
+
+        const resp = {
+            status: 0,
+            msg: "",
+            data: null,
+            errors: []
+        };
+
+        if(!req.headers['authorization']){
+            resp.errors.push({
+                location: "header",
+                param: "Authorization",
+                msg: "A Session ID precisa ser informada!"
+            });
+            return res.status(403).send(resp);
+        }
+
+        const session = await Sessao.Verificar(req.headers['authorization']);
+
+        if(session.status == 0){
+            resp.errors.push(
+                {
+                    "location": "params",
+                    "param": "sid",
+                    "msg": session.msg
+                }
+            );
+            return res.status(403).send(resp);
+        }
+
+        let where = `usuario = '${session.usuario}'`;
+        where += (query.where) ? `AND ( ${ query.where } )` : "";
+
+        let order_by = (query.order_by) ? query.order_by : "";
+        let limit = (query.limit) ? query.limit : "";
+
+        const clientes = await Cliente.Get(where, order_by, limit);
+
+        resp.status = 1;
+        resp.data = clientes || [];
+        res.send(resp);
+    });
+
 };
