908e755bc3f668d05ede1b35b892dd9957d0204c
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-05-23 16:21:32 -0300

    added: Verifica Sessão

--- classes/Sessao.js
+++ classes/Sessao.js
@@ -7,12 +7,35 @@ class Sessao extends Classes
         const data = {
             id: Util.generateId(),
             usuario: usuario,
+            token: Util.MD5(usuario + (new Date())),
             ultima_data: (new Date() / 1000 | 0)
         };
 
         const create = await this.Create(data);
 
         return (create.status === 1) ? data : false;
+    }
+
+    static async Verificar(id){
+        let sessao = await this.Get(`id = '${id}'`);
+
+        if(sessao.length === 0){
+            return {
+                status: 0,
+                msg: "Sessão inválida!"
+            };
+        }
+
+        sessao = sessao[0];
+
+        this.Update({ ultima_data: now }, `token = '${token}'`);
+        
+        return{
+            status: 1,
+            msg: "Sessão válida",
+            data: sessao
+        };
+
     }
 }
 
