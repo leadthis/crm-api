68120747f5693deddbac9740df25e38f184131c4
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-05-23 11:38:40 -0300

    updated: Config

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,12 +3,16 @@ module.exports = (app) => {
     // [GET] => /user
     app.get("/user", async (req, res) => {});
     
+    // [GET] => /user/:id
     app.get("/user/:id", async (req, res) => {});
     
+    // [PUT] => /user/:id
     app.put("/user/:id", async (req, res) => {});
     
+    // [POST] => /user
     app.post("/user", async (req, res) => {});
     
+    // [DELETE] => /user
     app.delete("/user/:id", async (req, res) => {});
 
 };
