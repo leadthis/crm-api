37de9e85ef2a9b57fc8116ba3e603c8940019bf3
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-05-30 15:56:33 -0300

    added: NodeEnv

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 global.PROD = (process.env.NODE_ENV == "prod");
 const app = require("./config")();
 
+require('dotenv').config({  
+    path: ".env"
+})
+
 const port = (PROD) ? 3000 : 3333;
 
 app.listen(port, async () => {
     console.clear();
     console.log(`Rodando na porta ${port}`);
-
+    
     const Jobs = require('./Jobs');
     Jobs();
 });
