b3dd00d3f72f7d0c6c9f1f75415a9c7f9b8d6c12
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-06-15 13:18:59 -0300

    updated: Expose Headers

--- config.js
+++ config.js
@@ -19,6 +19,7 @@ module.exports = () => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+        res.header('Access-Control-Expose-Headers', '*');
         // res.header('Referrer-Policy', 'strict-origin-when-cross-origin');
         next();
     });
